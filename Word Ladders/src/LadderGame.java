9d271f467706905d18fbf04ed24c95d48eda6e9f
Joshua Hamby <joshua.hamby@aggiemail.usu.edu>
2020-01-16 23:10:45 -0700

    first commit

---
+++ Word Ladders/src/LadderGame.java
@@ -0,0 +1,64 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Scanner;
+import java.util.Random;
+import java.io.File;
+import java.util.ArrayList;
+public class LadderGame<E> {
+    static int MaxWordSize = 15;
+    ArrayList<String>[] allList;
+    Random random;
+    E element;
+    Node<E> next;
+
+    public LadderGame(String file){
+        random = new Random();
+        allList = new ArrayList[MaxWordSize];
+        for( int i = 0; i < MaxWordSize; i++)
+            allList[i] = new ArrayList<String>();
+        try{
+            Scanner reader = new Scanner(new File(file));
+            while(reader.hasNext()){
+                String word = reader.next();
+                if (word.length()< MaxWordSize) allList[word.length()].add(word);
+           }
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public void play(String a, String b){
+        if (a.length() != b.length()){
+            System.out.println("Words are not the same length");
+            return;
+        }
+        if (a.length() >= MaxWordSize) return;
+        ArrayList list = new ArrayList();
+        ArrayList<String> l = allList[a.length()];
+        list = (ArrayList) l.clone();
+        System.out.println("Seeking a solution from " + a + " ->" + b + " Size of List " + list.size());
+
+    }
+    public void play(int len){
+        if (len >= MaxWordSize) return;
+        ArrayList<String> list = allList[len];
+        String a = list.get(random.nextInt(list.size()));
+        String b = list.get(random.nextInt(list.size()));
+        play(a, b);
+    }
+
+    public void listwords(int words, int length){
+        int count = 0;
+        for(String i : allList[length]){
+            if (count < words){
+                System.out.println(i);
+                count++;
+            }
+        }
+    }
+    public LadderGame(E e){
+        element = e;
+    }
+}
---
+++ Word Ladders/src/TestLadder.java
@@ -0,0 +1,15 @@
+public class TestLadder{
+    public static void main(String[] args) {
+        int RANDOMCT = 7;
+        LadderGame g = new LadderGame("dictionary.txt");
+        g.listwords(10, 6);
+        g.play("oops", "tots");
+//        g.play("ride", "ands");
+//        g.play("happily", "angrily");
+//        g.play("slow", "fast");
+//        g.play("stone", "money");
+//        g.play("biff", "axal");
+//        for (int i = 3; i < RANDOMCT; i++)
+//            g.play(i);
+    }
+}
